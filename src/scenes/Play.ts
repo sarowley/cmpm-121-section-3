bbd1eb0a5b37d65a3224ca9839ff9eaba4de740e
sarowley <sarowley@ucsc.edu>
2023-10-18 11:53:41 -0700

    done with second partner

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -12,6 +12,8 @@ export default class Play extends Phaser.Scene {
   starfield?: Phaser.GameObjects.TileSprite;
   spinner?: Phaser.GameObjects.Shape;
   enemy1?: Phaser.GameObjects.Shape;
+  enemy2?: Phaser.GameObjects.Shape;
+  enemy3?: Phaser.GameObjects.Shape;
 
   constructor() {
     super("play");
@@ -46,14 +48,24 @@ export default class Play extends Phaser.Scene {
 
     this.spinner = this.add.rectangle(640 / 2, 470, 15, 15, 0xb300db);
     this.enemy1 = this.add.rectangle(640, 270, 60, 40, 0xb300db);
+    this.enemy2 = this.add.rectangle(700, 200, 60, 40, 0xb300db);
+    this.enemy3 = this.add.rectangle(760, 130, 60, 40, 0xb300db);
   }
 
   update() {
     this.starfield!.tilePositionX -= 4;
     this.enemy1!.x -= 2;
+    this.enemy2!.x -= 2;
+    this.enemy3!.x -= 2;
 
     if (this.enemy1!.x == -40) {
       this.enemy1!.x = 640;
+    }
+    if (this.enemy2!.x == -40) {
+      this.enemy2!.x = 700;
+    }
+    if (this.enemy3!.x == -40) {
+      this.enemy3!.x = 760;
     }
 
     if (this.checkCollision(this.spinner!, this.enemy1!)) {
@@ -62,6 +74,22 @@ export default class Play extends Phaser.Scene {
       this.fired = false;
       this.enemy1!.x = 640;
       this.enemy1!.y = 270;
+    }
+
+    if (this.checkCollision(this.spinner!, this.enemy2!)) {
+      this.spinner!.x = 640 / 2;
+      this.spinner!.y = 470;
+      this.fired = false;
+      this.enemy2!.x = 700;
+      this.enemy2!.y = 200;
+    }
+
+    if (this.checkCollision(this.spinner!, this.enemy3!)) {
+      this.spinner!.x = 640 / 2;
+      this.spinner!.y = 470;
+      this.fired = false;
+      this.enemy3!.x = 760;
+      this.enemy3!.y = 130;
     }
 
     if (this.fired == true) {
